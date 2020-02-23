d5572d2dd73ce1176f5ca72f175277e1e81097d5
agungfebry <49956789+agungFebry@users.noreply.github.com>
2020-02-23 22:30:30 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,31 @@
-$(document).ready(function()
-    {
+$(function()
+{
+    // smooth scroll
     $(".page-scroll").on('click', function(event) 
-        {      
+    {      
         event.preventDefault();
 
         var hash = this.hash;
 
         $('html, body').animate(
-            {
+        {
             scrollTop: $(hash).offset().top - 100
-            }, 1000);
-        });
+        }, 1000);
     });
 
-$(function()
-{
-  $(".preloader").fadeOut(2000);
+// loading
+$(".loader").fadeOut(2500);
+
+// sidebar
 });
 
+$(function(){
+    $('.navbar-toggler').on('click', function () {
+        $(".navbar-collapse").fadeIn(500);
+    });
+
+    $('.icon-close').on('click', function () {
+        $(".navbar-collapse").fadeOut(500);
+    });
+
+});
